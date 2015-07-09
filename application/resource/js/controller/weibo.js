f04227d1fc85edb27e88562ee8b75ec17db57313
NanoShangguan <nano.shangguan@icloud.com>
2015-07-09 11:05:33 +0800

    finished two pie charts

--- application/resource/js/controller/weibo.js
+++ application/resource/js/controller/weibo.js
@@ -180,6 +180,45 @@ angular.module("weibo.controllers", [])
     })
     .controller('financialSentimentCtrl', ["$scope", function ($scope) {
         console.log("financialSentiment");
+        $scope.stockList = [
+            {name: "中国软件", id: 232345},
+            {name: "中国银行", id: 132345},
+            {name: "中国石油", id: 432345},
+            {name: "浦发银行", id: 265645},
+            {name: "中国软件", id: 100000}
+        ];
+
+        $scope.relatedWeibo = [{
+            img_url: "",
+            title: "资金面陷入“一钱难求”",
+            up: 400,
+            down: 200,
+            related: ["美的空调", "格力电器"]
+        }, {
+            img_url: "",
+            title: "资金面陷入“一钱难求”",
+            up: 300,
+            down: 20,
+            related: ["美的空调", "格力电器"]
+        },{
+            img_url: "",
+            title: "联通电商部总经理巴拉拉",
+            up: 200,
+            down: 50,
+            related: ["中国石化","中国石油"]
+        },  {
+            img_url: "",
+            title: "联通电商部总经理巴拉拉",
+            up: 200,
+            down: 50,
+            related: ["中国石化","中国石油"]
+        },  {
+            img_url: "",
+            title: "联通电商部总经理巴拉拉",
+            up: 200,
+            down: 450,
+            related: ["中国石化","中国石油"]
+        }];
         (function() {
             require.config({
                 paths: {
@@ -193,14 +232,18 @@ angular.module("weibo.controllers", [])
             require(
                 [
                     'echarts',
-                    'echarts/chart/tree'
+                    'echarts/chart/tree',
+                    'echarts/chart/pie'
                 ],
                 function (ec) {
                     // 基于准备好的dom，初始化echarts图表
                     var rn = ec.init(document.getElementById('relationship-network'));
                     var option = {
                         title : {
-                            text: '关联股票网'
+                            text: '关联股票网',
+                            textStyle: {
+                                fontSize: 16
+                            }
                         },
                         calculable : false,
 
@@ -358,10 +401,165 @@ angular.module("weibo.controllers", [])
                             }
                         ]
                     };
+
+                    var tep = ec.init(document.getElementById('topic-emotion-percent'));
+                    var option_for_tep = {
+                        title : {
+                            text: '话题情绪量比',
+                            x:'left',
+                            textStyle: {
+                                fontSize: 16
+                            }
+                        },
+                        tooltip : {
+                            trigger: 'item',
+                            formatter: "{b}</br>{c}</br>{d}%",
+                            showDelay: 0,
+                            //transitionDuration: 0,
+                            textStyle: {
+                                fontSize: 14
+                            }
+                        },
+                        legend: {
+                            // orient : 'vertical',
+                            x : 'left',
+                            y : 'bottom',
+                            selectedMode : false,
+
+                            data:[
+                                {name : '正面', icon : 'rectangle'},
+                                {name : '负面', icon : 'rectangle'},
+                                {name : '中性', icon : 'rectangle'}]
+                        },
+                        series : [
+                            {
+                                name:'话题情绪量比',
+                                type:'pie',
+                                clockWise: false,
+                                radius : '55%',
+                                center: ['35%', '50%'],
+                                itemStyle : {
+                                    normal : {
+                                        label : {
+                                            show: false
+                                        },
+                                        labelLine : {
+                                            show : false
+                                        },
+                                        borderWidth: 3,
+                                        borderColor: '#1B1B1B'
+                                    }
+                                },
+                                data:[
+                                    {value:335, name:'正面',
+                                        itemStyle : {
+                                            normal: {
+                                                color : '#FFDE59'
+                                            }
+                                        }},
+                                    {value:310, name:'负面',
+                                        itemStyle : {
+                                            normal: {
+                                                color : '#2C4773'
+                                            }
+                                        }},
+                                    {value:234, name:'中性',
+                                        itemStyle : {
+                                            normal: {
+                                                color : '#69BFDE'
+                                            }
+                                        }},
+                                ]
+                            }
+                        ]
+                    };
+
+                    var tif = ec.init(document.getElementById('topic-involved-fans'));
+                    var option_for_tif = {
+                        title : {
+                            text: '话题讨论用户粉丝数分析图',
+                            x:'left',
+                            textStyle: {
+                                fontSize: 16
+                            }
+                        },
+                        tooltip : {
+                            trigger: 'item',
+                            formatter: "{c}</br>{d}%",
+                            showDelay: 0,
+                            //transitionDuration: 0,
+                            textStyle: {
+                                fontSize: 14
+                            }
+                        },
+                        legend: {
+                            // orient : 'vertical',
+                            x : 'left',
+                            y : 'bottom',
+                            selectedMode : false,
+
+                            data:[
+                                {name : '1-1000', icon : 'rectangle'},
+                                {name : '1000-5000', icon : 'rectangle'},
+                                {name : '5000-1万', icon : 'rectangle'},
+                                {name : '1万以上粉丝', icon : 'rectangle'}]
+                        },
+                        series : [
+                            {
+                                name:'粉丝数',
+                                type:'pie',
+                                clockWise: false,
+                                radius : '55%',
+                                center: ['35%', '50%'],
+                                itemStyle : {
+                                    normal : {
+                                        label : {
+                                            show: false
+                                        },
+                                        labelLine : {
+                                            show : false
+                                        },
+                                        borderWidth: 3,
+                                        borderColor: '#1B1B1B'
+                                    }
+                                },
+                                data:[
+                                    {value:335, name:'1-1000',
+                                        itemStyle : {
+                                            normal: {
+                                                color : '#FFDE59'
+                                            }
+                                        }},
+                                    {value:310, name:'1000-5000',
+                                        itemStyle : {
+                                            normal: {
+                                                color : '#327cc0'
+                                            }
+                                        }},
+                                    {value:234, name:'5000-1万',
+                                        itemStyle : {
+                                            normal: {
+                                                color : '#2C4773'
+                                            }
+                                        }},
+                                    {value:234, name:'1万以上粉丝',
+                                        itemStyle : {
+                                            normal: {
+                                                color : '#69BFDE'
+                                            }
+                                        }}
+                                ]
+                            }
+                        ]
+                    };
                 require(['application/resource/plugins/echarts/theme/dark'], function(tarTheme){
                     rn.setTheme(tarTheme);
+                    tep.setTheme(tarTheme);
+                    tif.setTheme(tarTheme);
                 })
                 rn.setOption(option);
+                tep.setOption(option_for_tep);
+                tif.setOption(option_for_tif);
     }
 )
 })();
